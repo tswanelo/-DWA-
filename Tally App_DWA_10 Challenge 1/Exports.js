0db3cb52ee789ee97e69b93544ef7e113e091758
tswanelo28 <t.mokoena2@lab.clicklearning.org>
2023-08-30 23:04:29 +0200

    DWA_10 challenge 1 commit

--- Tally App_DWA_10 Challenge 1/Exports.js
+++ Tally App_DWA_10 Challenge 1/Exports.js
@@ -2,5 +2,5 @@
 
 export const MAX_NUMBER = 10;
 export const MIN_NUMBER = -10;
-export const STEP_AMOUNT = 1;
+export const STEP_AMOUNT = 1; // Increment the counter by one and Decrement the counter by one.
 
--- Tally App_DWA_10 Challenge 1/handlers.js
+++ Tally App_DWA_10 Challenge 1/handlers.js
@@ -1,37 +1,57 @@
 // handlers.js
 
+// Importing constants from 'Exports.js'
 import { STEP_AMOUNT, MIN_NUMBER, MAX_NUMBER } from './Exports.js';
 
+// Handler function for subtract button
 export const subtractHandler = (number, subtract, add) => {
+  // Calculate the new value by subtracting the step amount
   const newValue = parseInt(number.value) - STEP_AMOUNT;
+  
+  // Update the input value with the new calculated value
   number.value = newValue;
 
+  // Enable the 'add' button since subtraction was performed
   if (add.disabled === true) {
     add.disabled = false;
   }
 
+  // Disable the 'subtract' button if the new value reaches or goes below the minimum allowed value
   if (newValue <= MIN_NUMBER) {
     subtract.disabled = true;
   }
 };
 
+// Handler function for add button
 export const addHandler = (number, subtract, add) => {
+  // Calculate the new value by adding the step amount
   const newValue = parseInt(number.value) + STEP_AMOUNT;
+
+  // Update the input value with the new calculated value
   number.value = newValue;
 
+  // Enable the 'subtract' button since addition was performed
   if (subtract.disabled === true) {
     subtract.disabled = false;
   }
 
+  // Disable the 'add' button if the new value reaches or goes beyond the maximum allowed value
   if (newValue >= MAX_NUMBER) {
     add.disabled = true;
   }
 };
 
+// Handler function for reset button
 export const resetHandler = (number, subtract, add) => {
+  // Reset the input value to 0
   number.value = 0;
+
+  // Disable the 'subtract' button as the value is now at its minimum
   subtract.disabled = true;
+
+  // Enable the 'add' button after reset
   add.disabled = false;
 
+  // Display an alert to notify that the counter has been reset
   alert("The counter has been reset.");
 };
